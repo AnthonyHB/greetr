805ce8a49209bf62be312216bc4fd8bcd02235aa
Anthony Benites <anthony.benites17@gmail.com>
2016-11-29 13:24:26 -0700

    add greeting functions

--- Greetr.js
+++ Greetr.js
@@ -4,13 +4,56 @@
 		return new Greetr.init(firstName, lastName, language);
 	}
 
+	var supportedLangs = ['en', 'es'];
+
+	var greetings = {
+		en: 'Hello',
+		es: 'Hola'
+	};
+
+	var formalGreetings = {
+		en: 'Greetings',
+		es: 'Saludos'
+	};
+
+	var logMessages = {
+		en: 'Logged in',
+		es: 'Inició sesión'
+	}
+
+	Greetr.prototype = {
+
+		fullName: function() {
+			return this.firstName + ' ' + this.lastName;
+		},
+
+		validate: function() {
+			if (supportedLangs.indexOf(this.language) === -1) {
+				throw "Invalid language";
+			}
+		},
+
+		greeting: function() {
+			return greetings[this.language] + ' ' + this.firstName + '!';
+		},
+
+		formalGreeting: function() {
+			return formalGreetings[this.language] + ', ' + this.fullName();
+		}
+
+	};
+
 	Greetr.init = function(firstName, lastName, language) {
 
 		var self = this;
-		self.firstname = firstName || '';
+		self.firstName = firstName || '';
 		self.lastName = lastName || '';
 		self.language = language || 'en';
 
 	}
+
+	Greetr.init.prototype = Greetr.prototype; 
+
+	global.Greetr = global.G$ = Greetr;
 
 }(window, jQuery));
